f1c704887090368fdcad2016d6c805775f2fb192
MuhamadAkmalAmran <akmal.amran52@gmail.com>
2024-09-23 11:44:29 +0800

    feat: auth middleware

--- handler/user.go
+++ handler/user.go
@@ -143,8 +143,9 @@ func (h *userHandler) UploadAvatar(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, response)
 		
 	}
-
-	userID := 1
+	
+	currentUser := c.MustGet("currentUser").(user.User)
+	userID := currentUser.ID
 
 	path := fmt.Sprintf("images/%d-%s", userID, file.Filename)
 
